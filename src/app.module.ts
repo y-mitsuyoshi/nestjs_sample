381da0c43167753a398b7e86530904eacb81123f
Yuma Mitsuyoshi <glorymanchesteru@gmail.com>
2023-03-07 08:08:25 +0900

    ディレクトリ修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UsersModule } from './users/users.module';
+import { UsersService } from './domains/users/users.service';
+import { UsersModule } from './domains/users/users.module';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
